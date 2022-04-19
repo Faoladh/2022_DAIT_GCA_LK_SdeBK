ae954c79e907e37a75c90723cdbb272acebf84d1
SwordsandSpectacles <wolfiewarrior@gmail.com>
2022-04-19 10:50:05 +0100

    Titled File, Added Data
    added in title comments, tried importing data.

---
+++ Dentistry_meditation_Pair_LK_SdeBK.R
@@ -0,0 +1,11 @@
+# Author:       Sadbh de Buitléir-Kearney, Leo Kenny
+# Title:        PgDip Immersive Technology Pair Project
+# Description:  Illustrating types, operators, constants, and functions 
+# Date:         19/04/2022
+# Version:      1.0
+# Note:         
+# See:          
+
+
+#Load Data from .csv file
+projectData <- read.csv("dentistry_meditation.csv")
